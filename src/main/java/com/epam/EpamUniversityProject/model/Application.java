63d94166ce1e51d55da6046451cd6641e73c26cc
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-22 14:36:13 +0200

    removed list of grades, changed setters

--- src/main/java/com/epam/EpamUniversityProject/model/Application.java
+++ src/main/java/com/epam/EpamUniversityProject/model/Application.java
@@ -1,12 +1,9 @@
 package com.epam.EpamUniversityProject.model;
 
-import java.util.List;
-
 public class Application {
     private long id;
     private Applicant applicant;
     private Faculty faculty;
-    private List<Grade> grades;
     private int priority;
     private ApplicationStatus status;
 
@@ -14,53 +11,39 @@ public class Application {
         return id;
     }
 
-    public Application setId(long id) {
+    public void setId(long id) {
         this.id = id;
-        return this;
     }
 
     public Applicant getApplicant() {
         return applicant;
     }
 
-    public Application setApplicant(Applicant applicant) {
+    public void setApplicant(Applicant applicant) {
         this.applicant = applicant;
-        return this;
     }
 
     public Faculty getFaculty() {
         return faculty;
     }
 
-    public Application setFaculty(Faculty faculty) {
+    public void setFaculty(Faculty faculty) {
         this.faculty = faculty;
-        return this;
-    }
-
-    public List<Grade> getGrades() {
-        return grades;
-    }
-
-    public Application setGrades(List<Grade> grades) {
-        this.grades = grades;
-        return this;
     }
 
     public int getPriority() {
         return priority;
     }
 
-    public Application setPriority(int priority) {
+    public void setPriority(int priority) {
         this.priority = priority;
-        return this;
     }
 
     public ApplicationStatus getStatus() {
         return status;
     }
 
-    public Application setStatus(ApplicationStatus status) {
+    public void setStatus(ApplicationStatus status) {
         this.status = status;
-        return this;
     }
 }
