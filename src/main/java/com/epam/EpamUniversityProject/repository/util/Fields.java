d0f7016ff92d059d6206cf601464b99a7499694a
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-24 16:04:27 +0200

    added new user fields

--- src/main/java/com/epam/EpamUniversityProject/repository/util/Fields.java
+++ src/main/java/com/epam/EpamUniversityProject/repository/util/Fields.java
@@ -6,6 +6,12 @@ public class Fields {
     public static final String USER_PASSWORD="password";
     public static final String USER_IS_BLOCKED ="is_banned";
     public static final String USER_ROLE="role";
+    public static final String USER_FIRST_NAME="first_name";
+    public static final String USER_LAST_NAME="last_name";
+    public static final String USER_FATHERS_NAME="fathers_name";
+    public static final String USER_REGION="region";
+    public static final String USER_CITY="city";
+    public static final String USER_SCHOOL="school";
 
     public static final String SUBJECT_ID="id";
     public static final String SUBJECT_NAME="name";
@@ -16,5 +22,5 @@ public class Fields {
     public static final String FACULTY_TOTAL_PLACES="total_places";
 
     public static final String GRADE_ID="id";
-
+    public static final String GRADE_SCORE="score";
 }
