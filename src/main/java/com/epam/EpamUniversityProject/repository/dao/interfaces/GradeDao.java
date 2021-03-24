72dffa632c855a3548957ea493bad04474844f19
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-24 16:06:11 +0200

    added GradeDao.getUsersGrades method

--- src/main/java/com/epam/EpamUniversityProject/repository/dao/interfaces/GradeDao.java
+++ src/main/java/com/epam/EpamUniversityProject/repository/dao/interfaces/GradeDao.java
@@ -2,8 +2,10 @@ package com.epam.EpamUniversityProject.repository.dao.interfaces;
 
 import com.epam.EpamUniversityProject.model.Grade;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface GradeDao extends Dao{
-    List<Grade> getUsersGrades(long id);
+    List<Grade> getUsersGrades(long id) throws SQLException;
 }
