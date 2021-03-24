94d63a6fdbfb101321426b7193ab11cb435e4dc2
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-24 16:06:35 +0200

    implemented GradeDao.getUsersGrades method

--- src/main/java/com/epam/EpamUniversityProject/repository/dao/impl/GradeDaoPostgresImpl.java
+++ src/main/java/com/epam/EpamUniversityProject/repository/dao/impl/GradeDaoPostgresImpl.java
@@ -1,29 +1,46 @@
 package com.epam.EpamUniversityProject.repository.dao.impl;
 
 import com.epam.EpamUniversityProject.model.Grade;
+import com.epam.EpamUniversityProject.model.Subject;
 import com.epam.EpamUniversityProject.repository.dao.interfaces.GradeDao;
+import com.epam.EpamUniversityProject.repository.util.DBManager;
+import com.epam.EpamUniversityProject.repository.util.Fields;
 import com.epam.EpamUniversityProject.repository.util.Mapper;
+import org.apache.log4j.Logger;
 
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GradeDaoPostgresImpl implements GradeDao {
+    private final GradeMapper mapper = new GradeMapper();
+    private final Logger log = Logger.getLogger(GradeDaoPostgresImpl.class);
+    private static final String SQL_GET_ALL_BY_USER =
+            "select grade.id,s.name,grade.score,s.id from grade" +
+                    " join subject s on grade.subject = s.id where \"user\"=?;";
 
-    private static final String SQL_GET_ALL_BY_USER="select * from grade where user=?;";
     @Override
-    public void add(Object item) throws IOException, SQLException {
+    public void add(Object item) throws SQLException {
 
     }
 
     @Override
-    public Object get(long id) throws IOException, SQLException {
+    public Object get(long id) throws  SQLException {
+        Connection connection=null;
+        PreparedStatement statement=null;
+        try {
+            connection=DBManager.getInstance().getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
-    public void update(Object newItem) throws IOException, SQLException {
+    public void update(Object newItem) throws SQLException {
 
     }
 
@@ -33,20 +50,53 @@ public class GradeDaoPostgresImpl implements GradeDao {
     }
 
     @Override
-    public List getAll() throws IOException, SQLException {
+    public List getAll() throws SQLException {
         return null;
     }
 
     @Override
-    public List<Grade> getUsersGrades(long id) {
-        return null;
+    public List<Grade> getUsersGrades(long userId) throws  SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        List<Grade> grades = new ArrayList<>();
+        try {
+            connection = DBManager.getInstance().getConnection();
+            statement = connection.prepareStatement(SQL_GET_ALL_BY_USER);
+            statement.setLong(1, userId);
+            rs = statement.executeQuery();
+            while (rs.next()) {
+                grades.add(mapper.mapRow(rs));
+            }
+            log.info("getUsersGrades->success");
+        } catch (SQLException e) {
+            log.error("getUsersGrades",e);
+            throw e;
+        } finally {
+            DBManager.getInstance()
+                    .close(rs)
+                    .close(statement)
+                    .close(connection);
+        }
+        return grades;
     }
 
-    private class GradeMapper implements Mapper<Grade>{
+    private class GradeMapper implements Mapper<Grade> {
 
         @Override
         public Grade mapRow(ResultSet resultSet) {
-            return null;
+            Grade grade = null;
+            try {
+                grade = new Grade();
+                grade.setId(resultSet.getLong(Fields.GRADE_ID));
+                grade.setScore(resultSet.getInt(Fields.GRADE_SCORE));
+                Subject subject = new SubjectDaoPostgresImpl
+                        .SubjectMapper().mapRow(resultSet);
+                grade.setSubject(subject);
+            } catch (SQLException e) {
+                log.error("mapRow",e);
+            }
+            return grade;
         }
     }
 }
