a186b76b423466b71138d9b215da13a94940225b
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-25 19:39:36 +0200

    added faculties list ot homepage of user

--- src/main/java/com/epam/EpamUniversityProject/web/servlet/applicant/UserHomeServlet.java
+++ src/main/java/com/epam/EpamUniversityProject/web/servlet/applicant/UserHomeServlet.java
@@ -1,4 +1,52 @@
 package com.epam.EpamUniversityProject.web.servlet.applicant;
 
-public class UserHomeServlet {
+import com.epam.EpamUniversityProject.model.Faculty;
+import com.epam.EpamUniversityProject.repository.dao.impl.FacultyDaoPostgresImpl;
+import com.epam.EpamUniversityProject.repository.dao.interfaces.FacultyDao;
+import com.epam.EpamUniversityProject.web.utils.Paths;
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(Paths.URL_APPLICANT_HOME)
+public class UserHomeServlet extends HttpServlet {
+    private FacultyDao facultyDao;
+    private Logger log=Logger.getLogger(UserHomeServlet.class);
+
+    @Override
+    public void init() throws ServletException {
+        facultyDao = new FacultyDaoPostgresImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        long userId = (long) session.getAttribute("user");
+        log.info("doGet->users id="+userId);
+        try {
+            List<Faculty> faculties = facultyDao.getAll();
+            log.info("doGet->retrieve faculties");
+            req.setAttribute("faculties", faculties);
+            req.getServletContext()
+                    .getRequestDispatcher(Paths.PAGE_APPLICANT_HOME)
+                    .forward(req, resp);
+        } catch (SQLException e) {
+            log.error("doGet->",e);
+            resp.sendRedirect(Paths.URL_ERROR);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+    }
 }
--- src/main/webapp/jsp/applicant/applicantHome.jsp
+++ src/main/webapp/jsp/applicant/applicantHome.jsp
@@ -1,13 +1,25 @@
     <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+        <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
         <!DOCTYPE html>
         <html lang="en">
         <head>
         <title>Login V15</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         </head>
         <body>
         <h1>Applicant</h1>
+            <table>
+                <tr>faculty </tr>
+                <tr>budget places </tr>
+                <tr>all places </tr>
+                <c:forEach items="${faculties}" var="faculty">
+                        <tr>
+                            <td>${faculty.name}</td>
+                            <td>${faculty.budgedPlaces}</td>
+                            <td>${faculty.totalPlaces}</td>
+                        </tr>
+                </c:forEach>
+            </table>
         </body>
         </html>
