09c9a4f4856b59849c73704b4e94c67357939f5a
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-25 15:20:21 +0200

    added

---
+++ src/main/java/com/epam/EpamUniversityProject/web/utils/Paths.java
@@ -0,0 +1,18 @@
+package com.epam.EpamUniversityProject.web.utils;
+
+public final class Paths {
+    public static final String PAGE_ERROR="/jsp/error.jsp";
+    public static final String PAGE_LOG_IN="/jsp/logIn.jsp";
+    public static final String PAGE_SIGN_UP="/jsp/signUp.jsp";
+    public static final String PAGE_APPLICANT_HOME ="/jsp/applicant/applicantHome.jsp";
+    public static final String PAGE_ADMIN_HOME="/jsp/admin/adminHome.jsp";
+
+    public static final String URL_ERROR="/error";
+    public static final String URL_NONE="/";
+    public static final String URL_LOG_IN = "/logIn";
+    public static final String URL_SIGN_UP="/signUp";
+    public static final String URL_APPLICANT_NONE="/applicant";
+    public static final String URL_APPLICANT_HOME="/applicant/home";
+    public static final String URL_ADMIN_NONE="/admin";
+    public static final String URL_ADMIN_HOME="/admin/home";
+}
