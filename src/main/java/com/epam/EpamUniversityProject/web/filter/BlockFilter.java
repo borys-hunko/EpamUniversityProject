18ebcfff31d873858cf81e2d4d23021abeb106e7
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-26 16:02:14 +0200

    added

---
+++ src/main/java/com/epam/EpamUniversityProject/web/filter/BlockFilter.java
@@ -0,0 +1,63 @@
+package com.epam.EpamUniversityProject.web.filter;
+
+import com.epam.EpamUniversityProject.model.User;
+import com.epam.EpamUniversityProject.repository.dao.impl.UserDaoPostgresImpl;
+import com.epam.EpamUniversityProject.repository.dao.interfaces.UserDao;
+import com.epam.EpamUniversityProject.web.utils.Paths;
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+@WebFilter(value = {Paths.URL_ADMIN, Paths.URL_APPLICANT})
+public class BlockFilter implements Filter {
+    private UserDao dao;
+    private static final Logger log = Logger.getLogger(BlockFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        dao = new UserDaoPostgresImpl();
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        try {
+            //get user from db to check if he is blocked
+            //because he an be blocked during session
+            User checkUser = dao.get(user.getId());
+            log.info("doFilter: retrieve user from db");
+            if (user.isBlocked()) {
+                log.info("doFilter: user is blocked");
+                request.setAttribute("errorMsg", "you are blocked");
+                request.getServletContext()
+                        .getRequestDispatcher(Paths.PAGE_ERROR)
+                        .forward(request, response);
+            } else {
+                log.info("doFilter: user isn't blocked");
+            }
+        } catch (SQLException e) {
+            log.debug("doFilter->", e);
+            request.setAttribute("errorMsg", "oops... something went wrong");
+            request.getServletContext()
+                    .getRequestDispatcher(Paths.PAGE_ERROR)
+                    .forward(request, response);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
