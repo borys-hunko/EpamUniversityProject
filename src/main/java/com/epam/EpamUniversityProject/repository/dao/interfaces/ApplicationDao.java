db0712ecfe23c16b89aa657a4bc821188cb5f460
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-26 16:00:21 +0200

    getUsersApplication added in ApplicationDao

--- src/main/java/com/epam/EpamUniversityProject/repository/dao/interfaces/ApplicationDao.java
+++ src/main/java/com/epam/EpamUniversityProject/repository/dao/interfaces/ApplicationDao.java
@@ -2,7 +2,9 @@ package com.epam.EpamUniversityProject.repository.dao.interfaces;
 
 import com.epam.EpamUniversityProject.model.Application;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ApplicationDao extends Dao<Application>{
+    List<Application> getUsersApplication(long userID) throws SQLException;
 }
