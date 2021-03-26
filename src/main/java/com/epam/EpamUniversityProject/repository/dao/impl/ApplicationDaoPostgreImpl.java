f8354b32266ff8e4b6c27bf7e2501ef012d9928b
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-26 16:02:46 +0200

    getUsersApplication in ApplicationDaoPostgreImpl implemented

--- src/main/java/com/epam/EpamUniversityProject/repository/dao/impl/ApplicationDaoPostgreImpl.java
+++ src/main/java/com/epam/EpamUniversityProject/repository/dao/impl/ApplicationDaoPostgreImpl.java
@@ -1,9 +1,10 @@
 package com.epam.EpamUniversityProject.repository.dao.impl;
 
-import com.epam.EpamUniversityProject.model.Application;
-import com.epam.EpamUniversityProject.model.Grade;
+import com.epam.EpamUniversityProject.model.*;
 import com.epam.EpamUniversityProject.repository.dao.interfaces.ApplicationDao;
+import com.epam.EpamUniversityProject.repository.dao.interfaces.FacultyDao;
 import com.epam.EpamUniversityProject.repository.dao.interfaces.GradeDao;
+import com.epam.EpamUniversityProject.repository.dao.interfaces.UserDao;
 import com.epam.EpamUniversityProject.repository.util.DBManager;
 import com.epam.EpamUniversityProject.repository.util.Fields;
 import com.epam.EpamUniversityProject.repository.util.Mapper;
@@ -13,18 +14,25 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicationDaoPostgreImpl implements ApplicationDao {
     private final Logger log = Logger.getLogger(ApplicationDaoPostgreImpl.class);
-    private final Mapper<Application> mapper = new ApplicationMapper();
+    private final Mapper<Application> applicationMapper = new ApplicationMapper();
     private final DBManager manager = DBManager.getInstance();
     private GradeDao gradeDao;
+    private FacultyDao facultyDao;
+    private UserDao userDao;
     private final static String SQL_ADD_APPLICATION =
             "insert into application(\"user\",faculty,priority,status) values (?,?,?,?);";
+    private final static String SQL_GET_USERS_APPLICATIONS =
+            "select * from application where \"user\"=?;";
 
     public ApplicationDaoPostgreImpl() {
         gradeDao = new GradeDaoPostgresImpl();
+        userDao = new UserDaoPostgresImpl();
+        facultyDao = new FacultyDaoPostgresImpl();
     }
 
     @Override
@@ -61,6 +69,7 @@ public class ApplicationDaoPostgreImpl implements ApplicationDao {
 
     @Override
     public Application get(long id) throws SQLException {
+
         return null;
     }
 
@@ -79,18 +88,69 @@ public class ApplicationDaoPostgreImpl implements ApplicationDao {
         return null;
     }
 
+    @Override
+    public List<Application> getUsersApplication(long userID) throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        List<Application> applications = new ArrayList<>();
+        try {
+            connection = manager.getConnection();
+            statement = connection.prepareStatement(SQL_GET_USERS_APPLICATIONS);
+            statement.setLong(1, userID);
+            rs = statement.executeQuery();
+            while (rs.next()) {
+                Application application = applicationMapper.mapRow(rs);
+                Faculty faculty = facultyDao.get(rs.getLong(Fields.APPLICATION_FACULTY));
+                User user = userDao.get(rs.getLong(Fields.APPLICATION_USER));
+                List<Grade> grades = gradeDao.getApplicationGrades(application.getId());
+                application.setApplicant(user)
+                        .setFaculty(faculty)
+                        .setGrades(grades);
+                applications.add(application);
+            }
+        } catch (SQLException e) {
+            log.error("getUsersApplication->", e);
+            throw e;
+        } finally {
+            manager.close(rs)
+                    .close(statement)
+                    .close(connection);
+        }
+        return applications;
+    }
+
     private class ApplicationMapper implements Mapper<Application> {
 
         @Override
         public Application mapRow(ResultSet resultSet) {
-            Application application = null;
-//            try {
-////                application.setId(resultSet.getLong(Fields.APPLICATION_ID))
-////                        .setApplicant(null);
-////            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-            return null;
+            try {
+                log.info("mapRow->mappingStarted");
+                Application application=new Application();
+                application.setId(resultSet.getLong(Fields.APPLICATION_ID))
+                        .setPriority(resultSet.getInt(Fields.APPLICATION_PRIORITY))
+                        .setStatus(ApplicationStatus.valueOf(
+                                        resultSet.getString(Fields.APPLICATION_STATUS)
+                                )
+                        );
+                log.info("map->mappingFinished");
+                return application;
+            } catch (SQLException e) {
+                log.error("mapRow->",e);
+                return null;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ApplicationDao dao=new ApplicationDaoPostgreImpl();
+            List<Application> applications=dao.getUsersApplication(2);
+            for (Application a:applications){
+                System.out.println(a);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
