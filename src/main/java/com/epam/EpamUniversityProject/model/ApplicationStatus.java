b1216f9313f4a4c9bf3f492500e5b72ad1ffad2b
BorysHunko <borys.gennadiyovych.hunko@gmail.com>
2021-03-28 17:52:50 +0300

    removed BUDGET_APPLIED and CONTRACT_APPLIED

--- src/main/java/com/epam/EpamUniversityProject/model/ApplicationStatus.java
+++ src/main/java/com/epam/EpamUniversityProject/model/ApplicationStatus.java
@@ -1,5 +1,5 @@
 package com.epam.EpamUniversityProject.model;
 
 public enum ApplicationStatus {
-    NOT_PROCEED, NOT_APPLIED, BUDGET_APPLIED, CONTRACT_APPLIED
+    NOT_PROCEED, NOT_APPLIED, APPLIED
 }
